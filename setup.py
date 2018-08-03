55f6745c2035c65301f8bf234d49c562bdf6c7b2
Matthias L. Jugel <leo@thinkberg.com>
2018-08-03 16:46:28 +0200

    add support for pip installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ubirch-protocol",
+    version="0.0.1",
+    author="Matthias L. Jugel",
+    author_email="matthias.jugel@ubirch.com",
+    description="A ubirch-protocol implementation for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ubirch/ubirch-protocol-python",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)
