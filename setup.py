dfbfd6285eb9f599580cad31d64ef66be308fe1b
Matthias L. Jugel <leo@thinkberg.com>
2018-08-18 00:14:16 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ubirch-protocol",
-    version="1.0.0",
+    version="1.0.1",
     author="Matthias L. Jugel",
     author_email="matthias.jugel@ubirch.com",
     description="A ubirch-protocol implementation for python.",
