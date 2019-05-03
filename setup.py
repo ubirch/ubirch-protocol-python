3d04f0bf26c5dfcd714c56aea6c85756ed1f7c49
Matthias L. Jugel <leo@thinkberg.com>
2019-05-03 12:51:25 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ubirch-protocol",
-    version="2.0.0",
+    version="2.0.1",
     author="Matthias L. Jugel",
     author_email="matthias.jugel@ubirch.com",
     description="A ubirch-protocol implementation for python.",
