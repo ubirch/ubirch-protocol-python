48a4232634f471b29a1aeba529b824914b534d86
Roxana Meixner <roxana.meixner@gmail.com>
2020-04-08 12:21:36 +0200

    prettier output

--- examples/create-hash.py
+++ examples/create-hash.py
@@ -6,17 +6,16 @@ import sys
 if len(sys.argv) < 2:
     print("usage:")
     print(
-        """  python3 ./create-hash.py '{"id":"7100c024-1e8e-4007-b25f-c056397e12b8","store":"ubirch","value":3000}' """)
-    sys.exit(0)
+        """python3 ./create-hash.py '{"id": "605b91b4-49be-4f17-93e7-f1b14384968f", "ts": 1585838578, "data": 1234567890}'""")
+    sys.exit()
 
-print(sys.argv[1])
+print("   input: " + sys.argv[1])
 message = json.loads(str(sys.argv[1]))
 
 # create a compact rendering of the message to ensure determinism when creating the hash
 serialized = json.dumps(message, separators=(',', ':'), sort_keys=True).encode()
+print("rendered: " + serialized.decode())
 
 # calculate hash of message
 message_hash = hashlib.sha256(serialized).digest()
-
-# compare hash with expected value
-print(base64.b64encode(message_hash).decode())
+print("    hash: " + base64.b64encode(message_hash).decode())
