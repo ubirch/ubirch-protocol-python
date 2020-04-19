fe4796353e3960d122369be520ed32a33cf106e6
Roxana Meixner <roxana.meixner@gmail.com>
2020-04-19 23:06:54 +0200

    [UP-1288] ensure correct utf-8 encoding when serializing json

--- examples/create-hash.py
+++ examples/create-hash.py
@@ -6,14 +6,14 @@ import sys
 if len(sys.argv) < 2:
     print("usage:")
     print(
-        """python3 ./create-hash.py '{"id": "605b91b4-49be-4f17-93e7-f1b14384968f", "ts": 1585838578, "data": 1234567890}'""")
+        """python3 ./create-hash.py '{"id": "605b91b4-49be-4f17-93e7-f1b14384968f", "ts": 1585838578, "data": 1234}'""")
     sys.exit()
 
 print("   input: " + sys.argv[1])
 message = json.loads(str(sys.argv[1]))
 
 # create a compact rendering of the message to ensure determinism when creating the hash
-serialized = json.dumps(message, separators=(',', ':'), sort_keys=True).encode()
+serialized = json.dumps(message, separators=(',', ':'), sort_keys=True, ensure_ascii=False).encode()
 print("rendered: " + serialized.decode())
 
 # calculate hash of message
