3351570d054ad71219f73f52c5a04c7d2730f96d
Roxana Meixner <roxana.meixner@gmail.com>
2020-09-08 12:39:27 +0200

    simplified example for ecdsa signature verification

--- examples/test-ecdsa.py
+++ examples/test-ecdsa.py
@@ -1,23 +1,22 @@
 #! /usr/bin/env python3
+import binascii
 import hashlib
 
 import ecdsa
 
-TEST_PRIV = bytes.fromhex("8f827f925f83b9e676aeb87d14842109bee64b02f1398c6dcdd970d5d6880937")
-TEST_PUBL = bytes.fromhex("55f0feac4f2bcf879330eff348422ab3abf5237a24acaf0aef3bb876045c4e532fbd6cd8e265f6cf28b46e7e4512cd06ba84bcd3300efdadf28750f43dafd771")
+# demo public key
+pubkey_base64 = "9vxNdELoMlz7BnbYQMW5P5pLIFwt/90lyCxXDYYMZArcSdxdTNnJZA+D3ZsCfeWOKfKYF1UAsntHpciGJHw5wA=="
 
-sk = ecdsa.SigningKey.from_string(TEST_PRIV, curve=ecdsa.NIST256p, hashfunc=hashlib.sha256)
-vk = ecdsa.VerifyingKey.from_string(TEST_PUBL, curve=ecdsa.NIST256p, hashfunc=hashlib.sha256)
+# message to verify
+message_hex = "9623c4100512254113214020922500000b5cef07c4409ddb45e494aaa4b6d0bb4154b8904daec942bef0fdfc37f110ad5cd062c679e7f24fc559df983426ccf9a4175c794f09cc86acb95356d2eff4f602759e7e533700c4205493ed9492117c40565084a88ad1ecc8f5159ae10a1ce47ab74da70f230bf161c440314691c79a3364c367ee5343a8918da70c27b6b6176fffe8b0c580d99734fb04734d4bdd6a2a93703d84df5891ddd7eca7ee4de2d6f257802a4e765cb17e304b"
 
-CHAINED_MESSAGES = [
-    bytearray(bytes.fromhex(
-        "9623c4106eac4d0b16e645088c4622e7451ea5a1c44000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ccee01c4401fdd132be93216aa844b1d1a0838f1664babfa1fedab7b25ce48d0c9e124480b6c76e1780e877bda664c6d5795b2f0fe6660f78a27afb1228d84f37e92839b94")),
-    bytearray(bytes.fromhex(
-        "9623c4106eac4d0b16e645088c4622e7451ea5a1c4401fdd132be93216aa844b1d1a0838f1664babfa1fedab7b25ce48d0c9e124480b6c76e1780e877bda664c6d5795b2f0fe6660f78a27afb1228d84f37e92839b94ccee02c440ecd0676ac764a3b939f34422dc2cf56ebc7bbe2bcb3c7febc606e5832c38f60392c43371be134df6cf99301108bc5f09f1d04763427e79379d25a6923c2fcfcc")),
-    bytearray(bytes.fromhex(
-        "9623c4106eac4d0b16e645088c4622e7451ea5a1c440ecd0676ac764a3b939f34422dc2cf56ebc7bbe2bcb3c7febc606e5832c38f60392c43371be134df6cf99301108bc5f09f1d04763427e79379d25a6923c2fcfccccee03c440d3fcc18a4bec661a803af456873efaea3962afb2ec16955c0f7054ae6b0a169f00d2d86a74976f299a270a3450798b3cc43b2162c036ba76d8244279b044211e"))
-]
+pubkey_bytes = binascii.a2b_base64(pubkey_base64)
+vk = ecdsa.VerifyingKey.from_string(pubkey_bytes, curve=ecdsa.NIST256p, hashfunc=hashlib.sha256)
 
-for i, message in enumerate(CHAINED_MESSAGES):
-    print("verifying message {}".format(i))
-    vk.verify(message[-64:], message[0:-66], hashfunc=hashlib.sha256)
+message_bytes = binascii.unhexlify(message_hex)
+
+try:
+    vk.verify(message_bytes[-64:], message_bytes[:-66])
+    print("Signature successfully verified")
+except Exception as e:
+    print(e)
