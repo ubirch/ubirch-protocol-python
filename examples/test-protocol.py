c6c8762ec228706f0aab2a7ba60b8e817fd73775
Roxana Meixner <roxana.meixner@gmail.com>
2020-09-17 16:56:01 +0200

    update example

--- examples/test-protocol.py
+++ examples/test-protocol.py
@@ -16,18 +16,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import atexit
 import binascii
 import configparser
+import hashlib
+import json
 import logging
 import pickle
-from datetime import datetime
-from uuid import UUID, uuid4
+import sys
+import time
+from uuid import UUID
 
 import requests
+from ed25519 import VerifyingKey
+from requests import codes
 
 import ubirch
-from ubirch.ubirch_protocol import UBIRCH_PROTOCOL_TYPE_REG
+from ubirch.ubirch_protocol import UBIRCH_PROTOCOL_TYPE_REG, UBIRCH_PROTOCOL_TYPE_BIN
 
 logging.basicConfig(format='%(asctime)s %(name)20.20s %(levelname)-8.8s %(message)s', level=logging.DEBUG)
 logger = logging.getLogger()
@@ -36,11 +40,24 @@ logger = logging.getLogger()
 ########################################################################
 # Implement the ubirch-protocol with signing and saving the signatures
 class Proto(ubirch.Protocol):
+    UUID_PROD = UUID(hex="10b2e1a4-56b3-4fff-9ada-cc8c20f93016")
+    PUB_PROD = VerifyingKey("ef8048ad06c0285af0177009381830c46cec025d01d86085e75a4f0041c2e690", encoding='hex')
 
     def __init__(self, key_store: ubirch.KeyStore, uuid: UUID) -> None:
         super().__init__()
         self.__ks = key_store
+
+        # check if the device already has keys or generate a new pair
+        if not keystore.exists_signing_key(uuid):
+            keystore.create_ed25519_keypair(uuid)
+
+        # check if the keystore already has the backend key for verification or insert verifying key
+        if not self.__ks.exists_verifying_key(self.UUID_PROD):
+            self.__ks.insert_ed25519_verifying_key(self.UUID_PROD, self.PUB_PROD)
+
+        # load last signature for device
         self.load(uuid)
+
         logger.info("ubirch-protocol: device id: {}".format(uuid))
 
     def persist(self, uuid: UUID):
@@ -54,12 +71,15 @@ class Proto(ubirch.Protocol):
                 signatures = pickle.load(f)
                 logger.info("loaded {} known signatures".format(len(signatures)))
                 self.set_saved_signatures(signatures)
-        except:
+        except FileNotFoundError:
             logger.warning("no existing saved signatures")
             pass
 
     def _sign(self, uuid: UUID, message: bytes) -> bytes:
         return self.__ks.find_signing_key(uuid).sign(message)
+
+    def _verify(self, uuid: UUID, message: bytes, signature: bytes):
+        return self.__ks.find_verifying_key(uuid).verify(signature, message)
 
 ########################################################################
 
@@ -69,10 +89,11 @@ config = configparser.ConfigParser()
 config.read('demo-device.ini')
 if not config.has_section('device'):
     config.add_section('device')
-    config.set('device', 'uuid', str(uuid4()))
+    uuid = input("Enter your UUID:")
+    config.set('device', 'uuid', uuid)
     auth = input("Enter your API authentication token:")
     config.set('device', 'auth', auth)
-    config.set('device', 'env', 'demo')
+    config.set('device', 'env', 'prod')
     config.set('device', 'debug', 'False')
     config.set('device', 'groups', '')
     with open('demo-device.ini', "w") as f:
@@ -90,11 +111,7 @@ logger.info("ENV  : {}".format(env))
 logger.info("DEBUG: {}".format(debug))
 
 # create a new device uuid and a keystore for the device
-keystore = ubirch.KeyStore(uuid.hex + ".jks", "test-keystore")
-
-# check if the device already has keys or generate a new pair
-if not keystore.exists_signing_key(uuid):
-    keystore.create_ed25519_keypair(uuid)
+keystore = ubirch.KeyStore("demo-device.jks", "keystore")
 
 # create new protocol
 proto = Proto(keystore, uuid)
@@ -112,24 +129,43 @@ if not api.is_identity_registered(uuid):
         logger.error("device registration failed: {}".format(uuid))
     logger.debug(r.content)
 
-# send data packages
+for i in range(3):
+    # create a message like being sent to the customer backend
+    # include an ID and timestamp in the data message to ensure a unique hash
+    message = {
+        "id": str(uuid),
+        "ts": int(time.time()),
+        "n": i
+    }
 
-# message 1 - binary message no payload interpretation
-msg = proto.message_chained(uuid, 0x00, bytearray([1, 2, 3, 4, 5]))
-logger.info(binascii.hexlify(msg))
-r = api.send(uuid, msg)
-logger.info("1: {}: {}".format(r.status_code, r.content))
+    # create a compact rendering of the message to ensure determinism when creating the hash
+    serialized = json.dumps(message, separators=(',', ':'), sort_keys=True, ensure_ascii=False).encode()
+    logger.info("message({}): {}".format(i, serialized.decode()))
 
-# message 2 - interpreted payload message chained
-msg = proto.message_chained(uuid, 0x53, {'ts': int(datetime.utcnow().timestamp()), 'v': 99})
-logger.info(binascii.hexlify(msg))
-r = api.send(uuid, msg)
-logger.info("2: {}: {}".format(r.status_code, r.content))
+    # hash the message
+    message_hash = hashlib.sha256(serialized).digest()
+    logger.info("message hash({}): {}".format(i, binascii.b2a_base64(message_hash).decode().rstrip("\n")))
 
-# message 3 (chained to message 1)
-msg = proto.message_chained(uuid, 0x53, {"ts": int(datetime.utcnow().timestamp()), "v": 100})
-logger.info(binascii.hexlify(msg))
-r = api.send(uuid, msg)
-logger.info("3: {}: {}".format(r.status_code, r.content))
+    # create a new chained protocol message with the message hash
+    upp = proto.message_chained(uuid, UBIRCH_PROTOCOL_TYPE_BIN, message_hash)
+    logger.info("UPP({}): {}".format(i, binascii.hexlify(upp).decode()))
 
-atexit.register(proto.persist, uuid)
+    # send chained protocol message to UBIRCH authentication service
+    r = api.send(uuid, upp)
+    if r.status_code == codes.ok:
+        logger.info("UPP sent. backend response: {}".format(binascii.hexlify(r.content).decode()))
+    else:
+        logger.error(
+            "sending UPP failed! response: ({}) {}".format(r.status_code, binascii.hexlify(r.content).decode()))
+        sys.exit(1)
+
+    # verify the backend response
+    try:
+        proto.message_verify(r.content)
+        logger.info("backend response signature verified")
+    except Exception as e:
+        logger.error("backend response signature verification failed! {}".format(repr(e)))
+        sys.exit(1)
+
+# save last signature
+proto.persist(uuid)
