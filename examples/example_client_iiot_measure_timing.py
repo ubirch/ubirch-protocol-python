3d147c21dc2d3bd97955ce46d23791ae1028f3e7
ub-docz <ub-docz@users.noreply.github.com>
2022-02-08 20:41:52 +0100

    PROFIT-91 minor: add time limit line

--- examples/example_client_iiot_measure_timing.py
+++ examples/example_client_iiot_measure_timing.py
@@ -120,4 +120,5 @@ for name in sorted(statistics,reverse=True):
         value_array.append(time_offset)
     plt.scatter(block_nr_array,value_array,label=name, marker=next(marker))
 plt.legend(loc="upper left")
+plt.axhline(y=1000, color='r', linestyle='-') # add 1000 ms limit as line
 plt.show()
