22c7c9186e9fd060981fd4f067ca0e1fab1614ba
Roxana Meixner <roxana.meixner@ubirch.com>
2022-08-24 13:51:32 +0200

    Revert "replace deprecated setup.py with pyproject.toml, add simple setup.py to keep compatibility with legacy builds"
    This reverts commit fbb9a199e7ae418e6b82665c66a51ccf1362c71c.

--- setup.py
+++ setup.py
@@ -1,3 +1,27 @@
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ubirch-protocol",
+    version="2.1.0",
+    author="Matthias L. Jugel",
+    author_email="matthias.jugel@ubirch.com",
+    description="A ubirch-protocol implementation for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ubirch/ubirch-protocol-python",
+    packages=setuptools.find_packages(exclude=['bin', 'docs', 'examples', 'tests*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'msgpack>=0.6.0',
+        'ed25519>=1.4',
+        'pyjks>=17.1.1',
+        'requests>=2.19.1'
+    ],
+)
