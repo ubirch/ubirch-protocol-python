f46711546ca6576b91a43ce92b591c434e36ed92
Roxana Meixner <roxana.meixner@ubirch.com>
2022-09-07 16:36:30 +0200

    extract assertPrerequisites function to reduce redundancy and enhance readability

--- build.sh
+++ build.sh
@@ -3,52 +3,44 @@
 python3 -m venv venv
 . ./venv/bin/activate
 
+function assertPrerequisites {
+  VERSION=v$(python setup.py --version 2>/dev/null)
+  TAGGED=$(git describe --exact-match HEAD 2>&1)
+  BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  # check that we are on the master branch where we do releases
+  if [ "$BRANCH" != "master" ]; then
+    (echo >&2 "ERROR: not master branch ($BRANCH), aborted.")
+    exit -1
+  fi
+  if [ "$VERSION" != "$TAGGED" ]; then
+    (echo >&2 "ERROR: version does not match tag: '$VERSION' != '$TAGGED', aborted.")
+    exit -1
+  fi
+}
+
 case $1 in
-  build)
-    pip --no-cache-dir install -r requirements.txt
-    PYTHONPATH=. python -m compileall ubirch tests examples
-    ;;
-  test)
-    pip --no-cache-dir install -r requirements.test.txt
-    python -m pytest --junit-xml test-report.xml tests
-    python -c 'import xml.dom.minidom; print(xml.dom.minidom.parse("test-report.xml").toprettyxml())' > /tmp/report.xml
-    mv /tmp/report.xml test-report.xml
-    ;;
-  package)
-    VERSION=v$(python setup.py --version 2>/dev/null)
-    TAGGED=$(git describe --exact-match HEAD 2>&1)
-    BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    # check that we are on the master branch where we do releases
-    if [ "$BRANCH" != "master" ]; then
-      (>&2 echo "ERROR: not master branch ($BRANCH), aborted.")
-      exit -1
-    fi
-    if [ "$VERSION" == "$TAGGED" ]; then
-      pip --no-cache-dir install wheel
-      ./bin/create_package.sh
-    else
-      (>&2 echo "ERROR: version does not match tag: '$VERSION' != '$TAGGED', aborted.")
-      exit -1
-    fi
-    ;;
-  push)
-    VERSION=v$(python setup.py --version 2>/dev/null)
-    TAGGED=$(git describe --exact-match HEAD 2>&1)
-    BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    # check that we are on the master branch where we do releases
-    if [ "$BRANCH" != "master" ]; then
-      (>&2 echo "ERROR: not master branch ($BRANCH), aborted")
-      exit -1
-    fi
-    if [ "$VERSION" == "$TAGGED" ]; then
-      pip --no-cache-dir install twine
-      twine upload dist/*
-    else
-      (>&2 echo "ERROR: version does not match tag: '$VERSION' != '$TAGGED', aborted.")
-      exit -1
-    fi
-    ;;
-  *)
-    echo "Usage: $0 { build | test | package | push }"
-    exit 1
+build)
+  pip --no-cache-dir install -r requirements.txt
+  PYTHONPATH=. python -m compileall ubirch tests examples
+  ;;
+test)
+  pip --no-cache-dir install -r requirements.test.txt
+  python -m pytest --junit-xml test-report.xml tests
+  python -c 'import xml.dom.minidom; print(xml.dom.minidom.parse("test-report.xml").toprettyxml())' >/tmp/report.xml
+  mv /tmp/report.xml test-report.xml
+  ;;
+package)
+  assertPrerequisites
+  pip --no-cache-dir install wheel
+  ./bin/create_package.sh
+  ;;
+push)
+  assertPrerequisites
+  pip --no-cache-dir install twine
+  twine upload dist/*
+  ;;
+*)
+  echo "Usage: $0 { build | test | package | push }"
+  exit 1
+  ;;
 esac
