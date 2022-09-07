9debc9377d950ce042c1f6f7c85d8e2e8cb81ef0
Roxana Meixner <roxana.meixner@ubirch.com>
2022-09-07 16:25:53 +0200

    cleanup files

--- examples/trackle.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-import msgpack
-import binascii
-
-line = open(sys.argv[1]).read().rstrip()
-print(repr(line))
-msg = binascii.unhexlify(line)
-print(repr(msgpack.unpackb(msg, raw=True)))
