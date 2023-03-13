8c1e8abf57b43d59d6f6d71588a8f5096913c392
Roxana Meixner <roxana.meixner@ubirch.com>
2023-03-13 18:04:20 +0100

    enable debug log for json key registration

--- ubirch/ubirch_api.py
+++ ubirch/ubirch_api.py
@@ -104,12 +104,12 @@ class API(object):
         @return The response from the server
         """
         if key_registration.startswith(b'{'):
-            return self._register_identity_json1((key_registration))
+            return self._register_identity_json1(key_registration)
         else:
             return self._register_identity_mpack(key_registration)
 
     def _register_identity_json1(self, key_registration: bytes) -> Response:
-        #logger.debug("register identity [json]: {}".format(key_registration))
+        logger.debug("register identity [json]: {}".format(key_registration.decode()))
         r = requests.post(self.get_url(KEY_SERVICE), data=key_registration)
         logger.debug("{}: {}".format(r.status_code, r.content))
         return r
