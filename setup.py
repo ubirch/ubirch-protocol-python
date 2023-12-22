50adb3f71985c8dffe5afe286ec775645b97cad8
gruenwaldi <waldemar.gruenwald@ubirch.com>
2023-12-22 18:06:48 +0100

    updated to v3.3

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ubirch-protocol",
-    version="3.2.0",
+    version="3.3.0",
     author="Matthias L. Jugel",
     author_email="matthias.jugel@ubirch.com",
     description="A ubirch-protocol implementation for python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ubirch/ubirch-protocol-python",
-    packages=setuptools.find_packages(exclude=['bin', 'docs', 'examples', 'tests*']),
+    packages=setuptools.find_packages(exclude=['bin', 'docs', 'examples', 'tools', 'tests*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
@@ -21,6 +21,7 @@ setuptools.setup(
     install_requires=[
         'msgpack>=0.6.0',
         'ed25519>=1.4',
+        'ecdsa>=0.7',
         'pyjks>=17.1.1',
         'requests>=2.19.1'
     ],
